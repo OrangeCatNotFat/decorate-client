7c229d2cbf4fc6f2de4f43e783dd31005db5a4c9
橘猫吃不胖 <2199696269@qq.com>
2022-08-04 23:23:35 +0800

    新增表单验证

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Input, Button, Form, Row, Col, message} from "antd";
-import {UserOutlined, LockOutlined} from "@ant-design/icons";
-import {HOST, PORT} from "../config/apiconfig";
+import { Input, Button, Form, Row, Col, message } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { HOST, PORT } from "../config/apiconfig";
 import "../css/login.css";
 import axios from "axios";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import logo from '../logo.png';
-import {Findpwd} from "./findpwd";
+import { Findpwd } from "./findpwd";
 
 // import AdminInfo from "../state/adminInfo";
 
@@ -29,7 +29,7 @@ class Login extends React.Component {
                     username: value.username,
                     password: value.password
                 })
-                axios.post(`${HOST}:${PORT}/admins/login`, {user: value})
+                axios.post(`${HOST}:${PORT}/admins/login`, { user: value })
                     .then(result => {
                         if (result.data.status === 200) { // 登录成功
                             if (result.data.role === 2) {
@@ -48,8 +48,8 @@ class Login extends React.Component {
                             message.error(result.data.msg);
                         }
                     }).catch(err => {
-                    console.log(err);
-                })
+                        console.log(err);
+                    })
             })
     }
 
@@ -59,61 +59,65 @@ class Login extends React.Component {
                 <div className={"loginPage"}>
                     <section className="login-content">
                         <div>
-                            <img src={logo} style={{width: "400px"}} alt="logo"/>
+                            <img src={logo} style={{ width: "400px" }} alt="logo" />
                         </div>
-                        <div style={{marginLeft: "20px"}}>
+                        <div style={{ marginLeft: "20px" }}>
                             <h2 className={"login-title"}>创客装修后台管理系统</h2>
                             <Form ref={this.loginFormRef}>
-                                <Row>
+                                <Row style={{ height: "70px" }}>
                                     <Col span={24}>
                                         <Form.Item
                                             name={"username"}
                                             rules={[  // 规则
                                                 {
                                                     required: true,
-                                                    message: '用户名不能为空'
+                                                    message: '电话号码不能为空'
+                                                },
+                                                {
+                                                    pattern: /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/,
+                                                    message: "请输入正确的电话号码"
                                                 }
                                             ]}
                                         >
                                             <Input
-                                                style={{borderRadius: "24px"}}
-                                                prefix={<UserOutlined style={{color: 'rgba(0,0,0,0.25)'}}/>}
-                                                placeholder={"请输入用户名"}
-                                                size={"large"}/>
+                                                style={{ borderRadius: "24px" }}
+                                                prefix={<UserOutlined style={{ color: 'rgba(0,0,0,0.25)' }} />}
+                                                placeholder={"请输入电话号码"}
+                                                size={"large"} />
                                         </Form.Item>
                                     </Col>
                                 </Row>
-                                <Row>
+                                <Row style={{ height: "70px" }}>
                                     <Col span={24}>
                                         <Form.Item name="password"
-                                                   rules={[
-                                                       {
-                                                           required: true,
-                                                           message: '密码不能为空'
-                                                       }
-                                                   ]}>
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: '登录密码不能为空'
+                                                },
+                                            ]}>
                                             <Input.Password
-                                                style={{borderRadius: "24px"}}
-                                                prefix={<LockOutlined style={{color: "rgba(0,0,0,0.25)"}}/>}
-                                                placeholder={"请输入密码"}
-                                                size={"large"}/>
+                                                style={{ borderRadius: "24px" }}
+                                                prefix={<LockOutlined style={{ color: "rgba(0,0,0,0.25)" }} />}
+                                                placeholder={"请输入登录密码"}
+                                                size={"large"} />
                                         </Form.Item>
                                     </Col>
                                 </Row>
                                 <Row>
                                     <Col span={24}>
-                                        <Button type={"primary"} style={{width: "100%", borderRadius: "24px"}}
-                                                size={"large"} onClick={this.login}>登录</Button>
+                                        <Button type={"primary"} style={{ width: "100%", borderRadius: "24px" }}
+                                            size={"large"} onClick={this.login}>登录</Button>
                                     </Col>
                                 </Row>
                             </Form>
-                            <div style={{float: "right", marginTop: "5px"}}>
+                            <div style={{ float: "right", marginTop: "5px" }}>
                                 <Button type={"link"} onClick={() => this.props.history.push("/findPwd")}>忘记密码？</Button>
                             </div>
                         </div>
                     </section>
-                </div>
-            </BrowserRouter>
+                </div >
+            </BrowserRouter >
         )
     }
 
