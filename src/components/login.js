0c74cd310cb6646d0759de014d9cebedf0dacb39
橘猫吃不胖 <2199696269@qq.com>
2022-08-09 23:58:14 +0800

    增加redux管理

--- src/components/login.js
+++ src/components/login.js
@@ -4,14 +4,11 @@ import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { HOST, PORT } from "../config/apiconfig";
 import "../css/login.css";
 import axios from "axios";
-import { BrowserRouter } from "react-router-dom";
 import logo from '../logo.png';
 import md5 from 'js-md5'; // 引入md5加密算法
+import store from "../store";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     loginFormRef = React.createRef(); // 与render函数中的登录表单进行绑定
 
@@ -27,13 +24,20 @@ class Login extends React.Component {
                                 message.error("该账户已注销，如需启用请联系管理员");
                             } else {
                                 // 将用户名和真实姓名写入到sessionStorage中
-                                sessionStorage.setItem("username", value.username);
+                                // sessionStorage.setItem("username", value.username);
                                 // 将token写入到sessionStorage中
                                 sessionStorage.setItem("token", result.data.token);
                                 // 将身份写到页面上
-                                sessionStorage.setItem("role", result.data.role);
+                                // sessionStorage.setItem("role", result.data.role);
+                                const action = {
+                                    type: "user_login",
+                                    userData: result.data.data
+                                }
+                                store.dispatch(action); // 订阅
                                 // 进行页面跳转
-                                this.props.history.push("/home");
+                                setTimeout(() => {
+                                    this.props.history.push("/home");
+                                }, 0);
                             }
                         } else {
                             message.error(result.data.msg);
@@ -44,76 +48,78 @@ class Login extends React.Component {
             })
     }
 
-    render() {
-        return (
-            <BrowserRouter>
-                <div className={"loginPage"}>
-                    <section className="login-content">
-                        <div>
-                            <img src={logo} style={{ width: "400px" }} alt="logo" />
-                        </div>
-                        <div style={{ marginLeft: "20px" }}>
-                            <h2 className={"login-title"}>创客装修后台管理系统</h2>
-                            <Form ref={this.loginFormRef}>
-                                <Row style={{ height: "70px" }}>
-                                    <Col span={24}>
-                                        <Form.Item
-                                            name={"username"}
-                                            rules={[  // 规则
-                                                {
-                                                    required: true,
-                                                    message: '电话号码不能为空'
-                                                },
-                                                {
-                                                    pattern: /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/,
-                                                    message: "请输入正确的电话号码"
-                                                }
-                                            ]}
-                                        >
-                                            <Input
-                                                style={{ borderRadius: "24px" }}
-                                                prefix={<UserOutlined style={{ color: 'rgba(0,0,0,0.25)' }} />}
-                                                placeholder={"请输入电话号码"}
-                                                size={"large"} />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                                <Row style={{ height: "70px" }}>
-                                    <Col span={24}>
-                                        <Form.Item name="password"
-                                            rules={[
-                                                {
-                                                    required: true,
-                                                    message: '登录密码不能为空'
-                                                }
-                                            ]}>
-                                            <Input.Password
-                                                style={{ borderRadius: "24px" }}
-                                                prefix={<LockOutlined style={{ color: "rgba(0,0,0,0.25)" }} />}
-                                                placeholder={"请输入登录密码"}
-                                                size={"large"} />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col span={24}>
-                                        <Button type={"primary"} style={{ width: "100%", borderRadius: "24px" }}
-                                            size={"large"} onClick={this.login}>登录</Button>
-                                    </Col>
-                                </Row>
-                            </Form>
-                            <div style={{ float: "right", marginTop: "5px" }}>
-                                <Button type={"link"} onClick={() => this.props.history.push("/findPwd")}>忘记密码？</Button>
-                            </div>
-                        </div>
-                    </section>
-                </div >
-            </BrowserRouter >
-        )
+    componentDidMount() {
+        store.subscribe(() => {
+            store.getState();
+        })
     }
 
+    render() {
+        return (
+            <div className={"loginPage"}>
+                <section className="login-content">
+                    <div>
+                        <img src={logo} style={{ width: "400px" }} alt="logo" />
+                    </div>
+                    <div style={{ marginLeft: "20px" }}>
+                        <h2 className={"login-title"}>创客装修后台管理系统</h2>
+                        <Form ref={this.loginFormRef}>
+                            <Row style={{ height: "70px" }}>
+                                <Col span={24}>
+                                    <Form.Item
+                                        name={"username"}
+                                        rules={[  // 规则
+                                            {
+                                                required: true,
+                                                message: '电话号码不能为空'
+                                            },
+                                            {
+                                                pattern: /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/,
+                                                message: "请输入正确的电话号码"
+                                            }
+                                        ]}
+                                    >
+                                        <Input
+                                            style={{ borderRadius: "24px" }}
+                                            prefix={<UserOutlined style={{ color: 'rgba(0,0,0,0.25)' }} />}
+                                            placeholder={"请输入电话号码"}
+                                            size={"large"} />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Row style={{ height: "70px" }}>
+                                <Col span={24}>
+                                    <Form.Item name="password"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: '登录密码不能为空'
+                                            }
+                                        ]}>
+                                        <Input.Password
+                                            style={{ borderRadius: "24px" }}
+                                            prefix={<LockOutlined style={{ color: "rgba(0,0,0,0.25)" }} />}
+                                            placeholder={"请输入登录密码"}
+                                            size={"large"} />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={24}>
+                                    <Button type={"primary"} style={{ width: "100%", borderRadius: "24px" }}
+                                        size={"large"} onClick={this.login}>登录</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                        <div style={{ float: "right", marginTop: "5px" }}>
+                            <Button type={"link"} onClick={() => this.props.history.push("/findPwd")}>忘记密码？</Button>
+                        </div>
+                    </div>
+                </section>
+            </div >
+        )
+    }
 }
-
 
 export default Login;
 
