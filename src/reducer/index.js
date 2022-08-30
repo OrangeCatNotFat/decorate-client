dcfec3256bffe0760ae5aa6e9dd3ffdb28848e0d
橘猫吃不胖 <2199696269@qq.com>
2022-08-30 23:40:35 +0800

    reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,20 +1,22 @@
 // 定义初始状态
 const initState = {
-    userData: {}
+    userData: {}, // 存放用户自己的数据
+    key: 1, // 记录点击的侧边栏
+    caseRoute: [], // 记录案例管理的面包屑
 };
 
 // 判断action
 const reducer = (state = initState, action) => {
-    let newState = { ...state };// 获得初始数据
-    switch (action.type) {
-        case "user_login":
-            newState.userData = action.userData;
-            return newState; // 获取到了登录的数据
-        case "user_modify":
-            newState.userData=action.userData;
-        default:
-            return 1;
+    let newState = { ...state }; // 获得初始数据
+    if (action.type === "user_login") { // 用户登录时的数据
+        newState.userData = action.userData;
+        return newState; // 获取到了登录的数据
     }
+    if (action.type === "switch_page") { // 用户切换侧边栏
+        newState.key = action.key;
+        return newState;
+    }
+    return state;
 }
 
 export default reducer;
