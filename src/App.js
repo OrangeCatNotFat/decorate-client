85f482d032941f4147cdc541cbfd97f3864d7b05
橘猫吃不胖 <2199696269@qq.com>
2022-08-30 23:44:03 +0800

    package.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import {BrowserRouter, Route, HashRouter, Switch} from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
 import Login from "./components/login";
 import Frame from "./components/frame";
-import {FindPwd} from "./components/findpwd";
+import { FindPwd } from "./components/findpwd";
 import PrivateRoute from "./routes/privateRoute";
 import NotFound from "./components/notfound";
 
 function App() {
     return (
-        <div className="App">
-            <HashRouter>
-                <Switch>
-                    <Route exact path={"/"} component={Login}></Route>
-                    <Route path={"/login"} component={Login}></Route>
-                    <Route path={"/findPwd"} component={FindPwd}></Route>
-                    <PrivateRoute path={"/home"} component={Frame}></PrivateRoute>
-                    <Route path={"*"} component={NotFound}></Route>
-                </Switch>
-            </HashRouter>
-        </div>
+            <div className="App">
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path={"/"} component={Login}></Route>
+                        <Route path={"/login"} component={Login}></Route>
+                        <Route path={"/findPwd"} component={FindPwd}></Route>
+                        <PrivateRoute path={"/home"} component={Frame}></PrivateRoute>
+                        <Route path={"*"} component={NotFound}></Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
     );
 }
 
